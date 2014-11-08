f47d3fafbb1a38a2f5237cb6249463650b457a31
owen-d <ow.diehl@gmail.com>
2014-11-07 20:37:42 -0800

    everything working

--- watchout.js
+++ watchout.js
@@ -7,7 +7,8 @@ var gameOptions = {
 
 var gameStats = {
   score: 0,
-  bestScore: 0
+  bestScore: 0,
+  collisions: 0
 }
 
 var axes = {
@@ -40,6 +41,13 @@ var drag = d3.behavior.drag()
 }
 );
 
+setInterval(function(){
+  gameStats.score++;
+  d3.selectAll('.current').data([gameStats.score]).text(function(d){return 'Current score: ' + d});
+},100);
+
+
+
 var player = d3.select('.gameBoard').selectAll('.player').data([{x: axes.x(50), y: axes.y(50)}]).enter()
                   .append('circle')
                   .attr('cx', function(d){return d.x})
@@ -55,7 +63,9 @@ var update = function(){
   var enemies = d3.select('.gameBoard').selectAll('.enemies')
                .data(createEnemies());
 
-  enemies.transition().duration(1500)
+  enemies.transition()
+                    .duration(1500)
+                    .tween('checker', tweener)
                     .attr('cx', function(d){return d.x})
                     .attr('cy', function(d){return d.y});
 
@@ -69,6 +79,42 @@ var update = function(){
          .style('stroke', 'blue')
          .style('stroke-width', 2)
          .attr('class', 'enemies');
+
+  };
+var checkCollision = function(enemy) {
+  // var enemies = d3.select('.gameBoard').selectAll('.enemies');
+  // for (var i = 0; i < enemies.length; i++) {
+    // console.log(separation);
+      return function(t){
+    var radiusSum = parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+
+    var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(player.attr('cx'));
+    var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(player.attr('cy'));
+    var separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+
+    if (separation < radiusSum) {
+        gameStats.collisions++;
+        return resetScore();
+      }
+    };
+
 };
+var tweener = function(){
+  var enemy = d3.select(this);
+  return checkCollision(enemy)
+};
+var resetScore = function(){
+  if (gameStats.score > gameStats.bestScore) {
+    gameStats.bestScore = gameStats.score;
+    d3.selectAll('.high').data([gameStats.bestScore]).text(function(d){return 'High score: ' + d});
+    }
+    gameStats.score=0;
+  d3.selectAll('.current').data([gameStats.score]).text(function(d){return 'Current score: ' + d});
+  d3.selectAll('.collisions').data([gameStats.collisions])
+                                   .text(function (d) {
+                                    return "Collisions: " + d;
+                                   });
+};
+
 update();
 setInterval(update, 1700);
